a2db75c34b686c2aa0cd9efab0df4358dcb2acd7
Javi Manzano <javi.manzano.oller@gmail.com>
2014-07-14 22:33:47 +0100

    Things things

---
+++ app/src/main/java/com/fuzzingtheweb/ribbit/InboxFragment.java
@@ -0,0 +1,20 @@
+package com.fuzzingtheweb.ribbit;
+
+import android.os.Bundle;
+import android.support.v4.app.ListFragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by javi on 12/07/2014.
+ */
+public class InboxFragment extends ListFragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_inbox, container, false);
+        return rootView;
+    }
+}
---
+++ app/src/main/java/com/fuzzingtheweb/ribbit/ParseConstants.java
@@ -0,0 +1,12 @@
+package com.fuzzingtheweb.ribbit;
+
+/**
+ * Created by javi on 12/07/2014.
+ */
+public final class ParseConstants {
+    // Class names
+
+    // Field names
+    public static final String KEY_USERNAME = "username";
+    public static final String KEY_FRIENDS_RELATION = "friendsRelation";
+}
